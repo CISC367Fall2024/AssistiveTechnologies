caca43ef4d8b5a2ced8c7b43ac1b916ddbd3e115
antoinegravat <antoineg@udel.edu>
2024-10-21 02:09:20 -0400

    adding everything to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TaskInput } from './TaskBreakdown/TaskBreakdown';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <TaskInput></TaskInput>
       </header>
     </div>
   );
